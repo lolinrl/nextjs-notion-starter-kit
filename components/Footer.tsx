4eb256022e86f147579f0cd3f2a31e7d60854983
lolinrl <87694248+lolinrl@users.noreply.github.com>
2023-05-13 16:52:40 +0800

    Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,13 +36,22 @@ export const FooterImpl: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
-             Copyright 2022 {config.author}</div>
+        <div
+             style={(
+              justifyContent:çenter',
+              alignItems:'center'
+              )}
+           >
 
-      <a href='https://hits.seeyoufarm.com'>
-        <img
-             style={( display:'blok',margin:'0px auto')}
-             src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Femo.ijann.com&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true'
-           />
+        <a href='https://hits.seeyoufarm.com'>
+          <img
+               style={( display:'blok',margin:'0px auto')}
+               src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Femo.ijann.com&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true'
+             />
+          </a>
+          <div>Copyright 2022 {config.author}</div>
+        </div>
+      </div>
         
       <div className={styles.settings}>
         {hasMounted && (
