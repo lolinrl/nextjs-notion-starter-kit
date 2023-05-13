082aff98148f4dc9005b21a0f05b64da0f2cfa21
lolinrl <87694248+lolinrl@users.noreply.github.com>
2023-05-13 16:43:00 +0800

    site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,16 +10,16 @@ export default siteConfig({
 
   // basic site info (required)
   name: '今日誓不emo！',
-  domain: 'lolinrl.vercel.app',
+  domain: 'emo.ijann.com',
   author: 'lolinrl',
 
   // open graph metadata (optional)
   description: '今日誓不emo！',
 
   // social usernames (optional)
-  //twitter: 'transitive_bs',
+  // twitter: 'transitive_bs',
   github: 'lolinrl',
-  //linkedin: 'fisch2',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   youtube: 'channel/UCqharwnmlHeYf2XUBj-CLkw', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '14b8c6e102ed4adf99f94e7d9a038eec'
+    },
+    {
+      title: 'Contact',
+      pageId: '1c99a529d10442fd90f0ff75ff9fd164'
+    }
+  ]
 })
