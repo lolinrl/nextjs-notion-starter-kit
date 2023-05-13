4c993e58eb3b65dc2bc467b3bbd30bcee56ac091
lolinrl <87694248+lolinrl@users.noreply.github.com>
2023-05-13 16:36:50 +0800

    Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,8 +35,15 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>
+             Copyright 2022 {config.author}</div>
 
+      <a href='https://hits.seeyoufarm.com'>
+        <img
+             style={( display:'blok',margin:'0px auto')}
+             src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Femo.ijann.com&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=true'
+           />
+        
       <div className={styles.settings}>
         {hasMounted && (
           <a
